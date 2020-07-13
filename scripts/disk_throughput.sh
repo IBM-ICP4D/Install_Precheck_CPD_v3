c1a1b2086fcc52b739b38a96d32876ce1f13c0a7
Sanjit Chakraborty <17136230+sanjitc@users.noreply.github.com>
2020-07-13 17:40:29 -0500

    Add files via upload

--- scripts/disk_throughput.sh
+++ scripts/disk_throughput.sh
@@ -1,10 +1,10 @@
-###       Checking Disk latency       ###
-### Called from disklatency_check.yml ###
+###       Checking Disk throughput       ###
+### Called from diskthroughput_check.yml ###
 #!/bin/bash
 
 CRIODOCKERINSTALLPATH=/var/lib/docker
 
-dd if=/dev/zero of=${CRIODOCKERINSTALLPATH}/testfile bs=512 count=1000 oflag=dsync &> output
+dd if=/dev/zero of=${CRIODOCKERINSTALLPATH}/testfile bs=1G count=1 oflag=dsync &> output
 
 res=$(cat output | tail -n 1 | awk '{print $6}')
 # writing this since bc may not be default support in customer environment
