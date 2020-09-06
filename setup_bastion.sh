f9aa0bbb31cf58b3f66e61562797fd37ddfecfc4
sanjitc <shankarmscs@gmail.com>
2020-09-05 20:14:43 -0700

    Added checks for ansible exists on bastion node

--- setup_bastion.sh
+++ setup_bastion.sh
@@ -4,9 +4,14 @@
 
 #!/bin/bash
 
-if [[ $1 == "-r" ]]; then
-   ansible-galaxy remove akirak.coreos-python
+if ! [[ -x "$(command -v ansible)" ]]; then
+   echo "Error: ansible is not installed. Use 'yum install ansible' to install it." >&2
+   exit 1
 else
-   ansible-galaxy install akirak.coreos-python
-   cat pypy/pypy-5.6-linux_x86_64-portable.tar.bz2.parta? > pypy/pypy-5.6-linux_x86_64-portable.tar.bz2
+   if [[ $1 == "-r" ]]; then
+      ansible-galaxy remove akirak.coreos-python
+   else
+      ansible-galaxy install akirak.coreos-python
+      cat pypy/pypy-5.6-linux_x86_64-portable.tar.bz2.parta? > pypy/pypy-5.6-linux_x86_64-portable.tar.bz2
+   fi
 fi
