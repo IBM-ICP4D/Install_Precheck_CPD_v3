e67386c9a6b95b734e8d82753f3bb946690e02b3
sanjitc <shankarmscs@gmail.com>
2020-09-08 15:54:30 -0700

    Automated move files between bastion and core nodes

--- setup_bastion.sh
+++ setup_bastion.sh
@@ -4,6 +4,20 @@
 
 #!/bin/bash
 
+### Setup Core Nodes
+setup_core()
+{
+   for node in `awk '/\[core\]/{f=1}f' hosts_openshift | awk '{if ($5 == "ansible_ssh_user=core") print $1}'`
+   do 
+      echo ""
+      echo Preparing: $node
+      scp pypy/pypy-5.6-linux_x86_64-portable.tar.bz2 iperf3-3.1.3-1.fc24.x86_64.rpm core@$node:~/
+      ssh core@$node tar xjvf pypy-5.6-linux_x86_64-portable.tar.bz2 > /dev/null
+      ssh core@$node sudo rpm-ostree install iperf3-3.1.3-1.fc24.x86_64.rpm
+   done
+}
+
+### Setup Bastion Node
 if ! [[ -x "$(command -v ansible)" ]]; then
    echo "Error: ansible is not installed. Use 'yum install ansible' to install it." >&2
    exit 1
@@ -11,7 +25,12 @@ else
    if [[ $1 == "-r" ]]; then
       ansible-galaxy remove akirak.coreos-python
    else
+      yum install -y python-netaddr
+      yum install -y iperf3
       ansible-galaxy install akirak.coreos-python
       cat pypy/pypy-5.6-linux_x86_64-portable.tar.bz2.parta? > pypy/pypy-5.6-linux_x86_64-portable.tar.bz2
+      setup_core
    fi
 fi
+
+
